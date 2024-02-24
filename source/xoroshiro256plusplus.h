166151afa6dc55f5a3cb248fb47b22af89e50705
Vicente Helano <vicentehelano@gmail.com>
2024-02-24 08:39:25 -0300

    Update xoroshiro256plusplus.h

--- source/xoroshiro256plusplus.h
+++ source/xoroshiro256plusplus.h
@@ -65,4 +65,30 @@ static inline double xoroshiro256plusplus_d(void) {
   return __xoroshiro256plusplus_d(x);
 }
 
+/* Marsaglia's polar method adapted from https://c-faq.com/lib/gaussian.html */
+static inline double nxoroshiro256plusplus_d()
+{
+	static double V1, V2, S;
+	static int phase = 0;
+	double X;
+
+	if (phase == 0) {
+		do {
+			double U1 = xoroshiro256plusplus_d();
+			double U2 = xoroshiro256plusplus_d();
+
+			V1 = 2 * U1 - 1;
+			V2 = 2 * U2 - 1;
+			S = V1 * V1 + V2 * V2;
+			} while(S >= 1 || S == 0);
+
+		X = V1 * sqrt(-2 * log(S) / S);
+	} else
+		X = V2 * sqrt(-2 * log(S) / S);
+
+	phase = 1 - phase;
+
+	return X;
+}
+
 #endif // XOROSHIRO256PLUSPLUS_H
